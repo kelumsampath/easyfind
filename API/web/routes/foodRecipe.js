ff34c6dde42fd54ea898e8723c53919a29958c42
kelum <edirisingheemks@gmail.com>
2018-06-26 13:38:55 +0530

    instralled cloudinary and upload/get image

--- API/web/routes/foodRecipe.js
+++ API/web/routes/foodRecipe.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cloudinary = require('cloudinary');
 
 const router = express.Router();
 
 router.get('/',(req,res)=>{
     res.send("Hello foodrecipe!");
   });
+
+  cloudinary.config({ 
+    cloud_name: 'defah2zln', 
+    api_key: '227588295695171', 
+    api_secret: 'H3qw5KLfuUeLAi6dYvhqXFKu3I8' 
+  });
+  router.get('/a',(req,res)=>{
+  cloudinary.uploader.upload("http://weknowyourdreams.com/images/nature/nature-12.jpg",function(result) { 
+    res.send(result);
+});
+  });
   
+  router.get('/b',(req,res)=>{
+    const img =cloudinary.image("piuipwj6i5vuzejf279j.jpg", { alt: "Sample Image" });
+    res.send(img);
+
+  });
+
+
+
   module.exports = router;
